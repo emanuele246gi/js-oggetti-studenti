ff06b942f337e09723cc2f19879941b25a761941
Emanuele <emanuele65b@gmail.com>
2021-06-14 16:04:19 +0200

    add secondo punto

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
 // Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
 
+var studente [
 
+    {
+        nome: "Emanuele",
+        conome: "Bongiovanni",
+        età: "19"
+    }
+]
 
 // Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
 
+var addStudente {
+    nome: "",
+    cognome: "",
+    età: ""
+}
 
+addStudente.nome = prompt("Inserisci il nome");
+addStudente.cognome = prompt("Inserisci il cognome");
+addStudente.età = prompt("Inserisci l'età");
 
 // Creare un array di oggetti di studenti.
 
