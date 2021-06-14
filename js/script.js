274e6a4d0fbd34a796e461981ccc5c92c46d93da
Emanuele <emanuele65b@gmail.com>
2021-06-14 17:11:08 +0200

    finishing project

--- js/script.js
+++ js/script.js
@@ -1,31 +1,43 @@
 // Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
 
+// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+
+// Creare un array di oggetti di studenti.
+
+// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+
+// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
 var studente = [
+
     {
         nome: "Emanuele",
         conome: "Bongiovanni",
         età: "19"
     }
+
 ];
 
-// Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
-
 var addStudente = {
+
     nome: "",
     cognome: "",
     età: ""
+
 };
 
 addStudente.nome = prompt("Inserisci il nome");
+
 addStudente.cognome = prompt("Inserisci il cognome");
+
 addStudente.età = prompt("Inserisci l'età");
 
-// Creare un array di oggetti di studenti.
+studente.push(addStudente)
 
+var demo = [];
+for (var i = 0; j < studente.length; j++){
 
+    demo.push(" " + studente[i].nome + " " + studente[i].cognome + " " + studente[i].età);
 
-// Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
-
-
-
-// Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+    document.getElementById("demo").innerHTML = demo;
+}
